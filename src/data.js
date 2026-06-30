7bb49e86e985abcfd91e8d16fc93ae30d01d4197
cooper-710 <cooprobinson7@gmail.com>
2026-06-30 19:08:04 -0400

    Update pitch data files

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export async function loadPitchData(options = {}) {
   const [mlbRes, aaaRes] = await Promise.all([
     fetch('./pitch_data.json'),
-    fetch('./aaa_pitch_data_2025-08-11_to_2025-08-18.json')
+    fetch('./aaa_pitch_data_2025-06-10_to_2025-06-28.json')
   ]);
   const [mlbData, aaaData] = await Promise.all([mlbRes.json(), aaaRes.json()]);
   if (!options.groupByLeague) return mlbData || {};
